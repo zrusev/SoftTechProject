69a2cc50a244fe7018743d8f75a4873823a9d050
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-04-24 11:44:06 +0300

    Register controller on front page + /register; routing fix; cache cleaned to fix the @method

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace MainBundle\Controller;
 
+use MainBundle\Entity\User;
+use MainBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,11 +13,37 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('security_login');
+
     }
+
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array('form' => $form->createView()));
+    }
+
 }
